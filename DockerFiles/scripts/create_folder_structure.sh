431e98c3aa4dfcec1e29e6f39eba2168883d26cd
Long Pham <9040472+longmpham@users.noreply.github.com>
2024-03-04 00:21:08 -0800

    updated create_folder script

--- DockerFiles/scripts/create_folder_structure.sh
+++ DockerFiles/scripts/create_folder_structure.sh
@@ -6,30 +6,32 @@ subfolders=("config" "data" "downloads" "media")
 
 # Create root folder with permissions 775
 root_folder="$HOME/Docker"
-mkdir -p "$root_folder"
-chmod 775 "$root_folder"
+mkdir -p "$root_folder" && chmod 775 "$root_folder"
 
-# Create subfolders with permissions 775
-for subfolder in "${subfolders[@]}"; do
-    mkdir -p "$root_folder/$subfolder"
-    chmod 775 "$root_folder/$subfolder"
-done
+# Create config and data folder with permissions 775
+config_folder="$root_folder/config"
+data_folder="$root_folder/data"
+mkdir -p "$config_folder" && chmod 775 "$config_folder"
+mkdir -p "$data_folder" && chmod 775 "$data_folder"
+
+# Create downloads and media folder with permissions 775
+downloads_folder="$data_folder/downloads"
+media_folder="$data_folder/media"
+mkdir -p "$downloads_folder" && chmod 775 "$downloads_folder"
+mkdir -p "$media_folder" && chmod 775 "$media_folder"
 
 # Create folders inside downloads with permissions 775
+downloads_folders=("downloading" "movies" "tvshows" "anime" "books" "music" "other" "torrent_files")
 for folder in "${folders[@]}"; do
-    mkdir -p "$root_folder/data/downloads/$folder"
-    chmod 775 "$root_folder/data/downloads/$folder"
+    mkdir -p "$downloads_folder/$folder" && chmod 775 "$downloads_folder/$folder"
 done
 
 # Create folders inside media with permissions 775
+media_folders=("movies" "tvshows" "anime" "books" "music" "other")
 for folder in "${folders[@]}"; do
-    mkdir -p "$root_folder/data/media/$folder"
-    chmod 775 "$root_folder/data/media/$folder"
+    mkdir -p "$media_folder/$folder"
+    chmod 775 "$media_folder/$folder"
 done
-
-# Additionally, create the 'downloading' folder inside 'downloads' with permissions 775
-mkdir -p "$root_folder/data/downloads/downloading"
-chmod 775 "$root_folder/data/downloads/downloading"
 
 # Display the folder structure
 echo "Folder structure created successfully with permissions set to 775."
